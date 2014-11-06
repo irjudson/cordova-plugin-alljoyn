c49bf1a5f1e905e8b5a6c8a3aa63b5148a9360e1
Ivan R. Judson <irjudson@gmail.com>
2014-11-05 22:06:39 -0800

    Fixing bugs.

--- src/android/AllJoyn.java
+++ src/android/AllJoyn.java
@@ -59,7 +59,7 @@ public class AllJoyn extends CordovaPlugin {
 				short contactPort = CONTACT_PORT;
 				SessionOpts sessionOpts = new SessionOpts();
 				Mutable.IntegerValue sessionId = new Mutable.IntegerValue();
-				// Status status = mBus.joinSession((String) msg.obj, contactPort, sessionId, sessionOpts, new SessionListener());
+				Status status = mBus.joinSession(name, contactPort, sessionId, sessionOpts, new SessionListener());
 			}
 		});
 
