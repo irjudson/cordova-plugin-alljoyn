b1c1d853190b23885239829926a9297c3542181d
Jason Poon <jason.poon@microsoft.com>
2014-11-11 13:42:23 -0800

    add discover method

--- www/alljoyn.js
+++ www/alljoyn.js
@@ -4,6 +4,11 @@ module.exports = {
 
     hello: function(callback, name) {
         exec(callback, null, "AllJoyn", "hello", [name]);
-    }
+    },
+
+    discover: function(success, error) {
+		argscheck.checkArgs('FF', 'allseen.alljoyn.discover', arguments);
+			exec(success, error, "AllJoyn", "discover", []);
+	}
 
 };
